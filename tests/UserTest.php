c56502c0119e5b25dfe1ee975c1baa728348df12
Fazle Elahee <felahee@gmail.com>
2020-05-01 22:41:05 +0100

    PHPUnit test with mockery

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -8,15 +8,27 @@ use WPCPlugin\DataSource\File;
 
 class UserTest extends TestCase
 {
-
     /**
      * @var $user User
      */
+
     private $user;
+
+    private $mockApi;
+
+    private $allUsersJson;
+
+    private $singleUserJson;
 
     public function setUp(): void
     {
-        $this->user = new User(new File(), __DIR__ . '/assets');
+        $this->mockApi = \Mockery::mock('WPCPlugin\Contracts\IDataSource');
+        $this->mockApi->shouldReceive('updatePath')
+            ->andReturnSelf();
+        $this->user = new User($this->mockApi, '/');
+
+        $this->allUsersJson = file_get_contents(__DIR__ . '/assets/users.json');
+        $this->singleUserJson =  file_get_contents(__DIR__ . '/assets/users/1.json');
     }
 
     public function testIsCreatedValidClass()
@@ -46,22 +58,35 @@ class UserTest extends TestCase
     public function testUserCollectionNotFoundException()
     {
         $this->expectException(\Exception::class);
+
+        $this->mockApi->shouldReceive('content')
+            ->andReturn("{}");
+
         $this->user->allUser("empty-users");
     }
 
     public function testUserNotFoundException()
     {
         $this->expectException(\Exception::class);
+
+        $this->mockApi->shouldReceive('content')
+            ->andReturn("{}");
+
         $this->user->findUserById(12);
     }
 
     public function testCanHaveExpectedNoOfArray()
     {
+        $this->mockApi->shouldReceive('content')
+            ->andReturn($this->allUsersJson);
         $this->assertEquals(10, count($this->user->allUser()));
     }
 
     public function testExpectedKeyExistsInUserArray()
     {
+        $this->mockApi->shouldReceive('content')
+            ->andReturn($this->singleUserJson);
+
         $user = $this->user->findUserById(1);
         $this->assertArrayHasKey('username', $user);
     }
@@ -69,5 +94,8 @@ class UserTest extends TestCase
     public function tearDown(): void
     {
         $this->user = null;
+        $this->mockApi = null;
+        $this->allUsersJson = '';
+        $this->singleUserJson = '';
     }
 }
